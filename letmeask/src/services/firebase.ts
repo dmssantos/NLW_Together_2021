59e5e007e598888b7ab455314c482d1815e6c58e
dmssantos <dms.santos_@hotmail.com>
2021-06-23 21:10:50 -0300

    update

--- letmeask/src/services/firebase.ts
+++ letmeask/src/services/firebase.ts
@@ -4,14 +4,15 @@ import 'firebase/auth'
 import 'firebase/database'
 
 const firebaseConfig = {
-  apiKey: process.env.RECT_APP_API_KEY,
-  authDomain: process.env.RECT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.RECT_APP_DATABASE_URL,
-  projectId: process.env.RECT_APP_PROJECT_ID,
-  storageBucket: process.env.RECT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.RECT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.RECT_APP_APP_ID
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
+
 
 firebase.initializeApp(firebaseConfig)
 
