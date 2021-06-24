d4664710b30ac483196134b5b8a89de4ec3996f9
dmssantos <dms.santos_@hotmail.com>
2021-06-24 15:27:14 -0300

    Context

--- letmeask/src/pages/Home.tsx
+++ letmeask/src/pages/Home.tsx
@@ -1,16 +1,27 @@
+import { useContext } from 'react'
 import { useHistory } from 'react-router-dom'
+
+import { AuthContext } from '../App'
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
+
 import { Button } from '../components/Button'
 
 import '../styles/auth.scss'
 
+
 export function Home(){
   const history = useHistory()
+  const { user, signInWithGoogle } = useContext(AuthContext)
+  
 
-  function navigateToNewRoom(){
+  async function handleCreateRoom(){
+    if(!user){
+      await signInWithGoogle()
+    }
+
     history.push('/rooms/new')
   }
 
@@ -22,9 +33,10 @@ export function Home(){
         <p>Tire as dúvidas da sua audiência em tempo-real</p>
       </aside>
       <main>
+        
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button onClick={navigateToNewRoom} className="create-room">
+          <button onClick={handleCreateRoom} className="create-room">
             <img src={googleIconImg} alt="Logo do Google" />
             Cria sua sala com o Google
           </button>
--- letmeask/src/pages/NewRoom.tsx
+++ letmeask/src/pages/NewRoom.tsx
@@ -1,12 +1,16 @@
+
 import { Link } from 'react-router-dom'
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import { Button } from '../components/Button'
 
+
 import '../styles/auth.scss'
 
 export function NewRoom(){
+  
+  
   return (
     <div id="page-auth">
       <aside>
